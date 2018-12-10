00787cff45c20c5467edbe37fdf633e440da8b46
Marczu <marczu09@gmail.com>
2018-12-10 19:50:46 +0100

    Init commit

---
+++ src/main/java/com/mejner/projectmanagertool/ProjectManagerToolApplication.java
@@ -0,0 +1,12 @@
+package com.mejner.projectmanagertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerToolApplication.class, args);
+    }
+}
