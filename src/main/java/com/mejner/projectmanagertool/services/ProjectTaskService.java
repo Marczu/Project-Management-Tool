8747cff15bed69773fc9a8c85624109fbdecbb95
Marczu <marczu09@gmail.com>
2019-01-17 15:59:24 +0100

    Add custom excepion for project tasks not found + for project not exist

--- src/main/java/com/mejner/projectmanagertool/services/ProjectTaskService.java
+++ src/main/java/com/mejner/projectmanagertool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.mejner.projectmanagertool.services;
 
 import com.mejner.projectmanagertool.domain.Backlog;
+import com.mejner.projectmanagertool.domain.Project;
 import com.mejner.projectmanagertool.domain.ProjectTask;
+import com.mejner.projectmanagertool.exceptions.ProjectNotFoundException;
 import com.mejner.projectmanagertool.repositories.BacklogRepository;
+import com.mejner.projectmanagertool.repositories.ProjectRepository;
 import com.mejner.projectmanagertool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,36 +21,51 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        try{
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 
-        projectTask.setBacklog(backlog);
+            projectTask.setBacklog(backlog);
 
-        //sequence is projectidentifier-PTSequence, for example ABCD-1 ABCD-2
-        Integer backlogSequence = backlog.getPTSequence();
-        backlogSequence++;
-        backlog.setPTSequence(backlogSequence);
+            //sequence is projectidentifier-PTSequence, for example ABCD-1 ABCD-2
+            Integer backlogSequence = backlog.getPTSequence();
+            backlogSequence++;
+            backlog.setPTSequence(backlogSequence);
 
-        //add Sequence to projectTask
-        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            //add Sequence to projectTask
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
 
-        projectTask.setProjectIdentifier(projectIdentifier);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        //SET initial priority when is null
-        if(projectTask.getPriority() == null){
-            projectTask.setPriority(3);
+            //SET initial priority when is null
+            if(projectTask.getPriority() == null){
+                projectTask.setPriority(3);
+            }
+
+            //SET initial status when is null
+            if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        }catch (Exception e){
+            throw new ProjectNotFoundException("Projekt nie został znaleziony");
         }
 
-        //SET initial status when is null
-        if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
-    public Iterable<ProjectTask> findBacklogById(String backlog_id) {
-        return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+    public Iterable<ProjectTask> findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if(project == null){
+            throw new ProjectNotFoundException("Projekt o ID '" + id + "' nie istnieje");
+        }
+
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
