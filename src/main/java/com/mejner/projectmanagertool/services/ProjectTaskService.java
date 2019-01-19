66ec25434af47e9153c07774597b0353fb4b92a4
Marczu <marczu09@gmail.com>
2019-01-19 12:21:51 +0100

    Fix for cascade type and delete project implementation

--- src/main/java/com/mejner/projectmanagertool/services/ProjectTaskService.java
+++ src/main/java/com/mejner/projectmanagertool/services/ProjectTaskService.java
@@ -98,11 +98,6 @@ public class ProjectTaskService {
     public void deletePTByProjectSequence(String backlog_id, String pt_id){
         ProjectTask projectTask = findPTBySequence(backlog_id, pt_id);
 
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = backlog.getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
-
         projectTaskRepository.delete(projectTask);
     }
 }
