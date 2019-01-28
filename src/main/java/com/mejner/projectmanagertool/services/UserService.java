3eabe6747db540bd1438c1e93d1cc8896ea94329
Marczu <marczu09@gmail.com>
2019-01-28 11:47:07 +0100

    Add user registration Service and controller

--- src/main/java/com/mejner/projectmanagertool/services/UserService.java
+++ src/main/java/com/mejner/projectmanagertool/services/UserService.java
@@ -1,7 +1,9 @@
 package com.mejner.projectmanagertool.services;
 
+import com.mejner.projectmanagertool.domain.User;
 import com.mejner.projectmanagertool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +12,11 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        return userRepository.save(newUser);
+    }
 }
