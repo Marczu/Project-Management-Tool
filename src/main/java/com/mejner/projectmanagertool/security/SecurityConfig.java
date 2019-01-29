61415e15f4c08a0ddbe9ed29694fc003985babfe
Marczu <marczu09@gmail.com>
2019-01-29 10:48:45 +0100

    Working Authentication filter

--- src/main/java/com/mejner/projectmanagertool/security/SecurityConfig.java
+++ src/main/java/com/mejner/projectmanagertool/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.mejner.projectmanagertool.security.SecurityConstants.H2_URL;
 import static com.mejner.projectmanagertool.security.SecurityConstants.SIGN_UP_URLS;
@@ -31,6 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -71,5 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
