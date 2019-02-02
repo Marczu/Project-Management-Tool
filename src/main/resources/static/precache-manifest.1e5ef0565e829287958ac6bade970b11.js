df06d7d25c6152c34f0f64888e39137c4c160155
Marczu <marczu09@gmail.com>
2019-02-02 15:37:41 +0100

    testing for heroku - add react statics

---
+++ src/main/resources/static/precache-manifest.1e5ef0565e829287958ac6bade970b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f72e98e3758ca9fb342",
+    "url": "/static/js/main.7f72e98e.chunk.js"
+  },
+  {
+    "revision": "df3b3450d5e3f3f3fe70",
+    "url": "/static/js/1.df3b3450.chunk.js"
+  },
+  {
+    "revision": "7f72e98e3758ca9fb342",
+    "url": "/static/css/main.e01f7034.chunk.css"
+  },
+  {
+    "revision": "df3b3450d5e3f3f3fe70",
+    "url": "/static/css/1.7fa62711.chunk.css"
+  },
+  {
+    "revision": "9e697c0b873f60d0ab8207660bc27754",
+    "url": "/index.html"
+  }
+];
